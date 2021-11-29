b43dece7f67ad61eb3603da7987efdf9e875dbed
waraponnnnn <630112418033@bru.ac.th>
2021-11-29 12:05:47 +0700

    LAB2

---
+++ Lab 2/es6-array.js
@@ -0,0 +1,23 @@
+const cars = ["TOYOTA ,HONDA,MAZDA"]
+const fruits = [];
+firuits [0] ="Apple";
+firuits [3] ="mango";
+const rname = new Array ("Win,John,James");
+console.log(frits);
+
+const carpop = cars.pop();
+console.log (carpop );
+console.log(cars);
+
+cars.push("kubota");
+console.log(cars);
+
+const carshift = cars.shift();
+console.log(carsshift);
+console.log(cars);
+
+cars.unshift("mazda");
+console.log(cars);
+
+const slideArr = cars.slice(0,2);
+console.log(slideArr);
---
+++ Lab 2/es6-basic.js
@@ -0,0 +1,26 @@
+var str = "Hello word !!!";  
+console.log(str);    
+
+let number = 10
+{   
+        let  number = 22;
+        console.log (number);
+}
+console.log(number);
+
+{
+    const number =10;   
+    // number =15;
+}
+
+function add(a,b) {
+    let c = a +b 
+    return c;
+    }
+let reault = add (5,3);
+console.log (reault );
+
+const addFn = (a,b) => a + b;
+reault =addFn(10,15);
+console.log(reault);
+
